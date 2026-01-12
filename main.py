771cb54e740b282ff3ab3a3f7a8871ddcc040579
GitFMarco <marco.frascerra.98@gmail.com>
2026-01-12 23:29:27 +0100

    [UPD] env

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from os import getenv
 
 
 if __name__ == "__main__":
-    with open("token.json") as f:
-        token = getenv("TELEGRAM_TOKEN")
-        if token is None:
-            raise Exception("Missing TELEGRAM_TOKEN environment variable. It needs to contain a valid telegram bot token.")
+    token = getenv("TELEGRAM_TOKEN")
+    if token is None:
+        raise Exception("Missing TELEGRAM_TOKEN environment variable. It needs to contain a valid telegram bot token.")
 
-        awake_paolo(token)
+    awake_paolo(token)
